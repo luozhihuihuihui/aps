f7b6cb5d558f6ee3af05a45467c86ca35c8afbe5
luozh <luozh@caskd.ac.cn>
2021-10-11 17:45:36 +0800

    mq

---
+++ aps-mq/src/main/java/com/hithy/apsmq/ApsMqApplication.java
@@ -0,0 +1,13 @@
+package com.hithy.apsmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApsMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApsMqApplication.class, args);
+    }
+
+}
---
+++ aps-mq/src/test/java/com/hithy/apsmq/ApsMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hithy.apsmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
